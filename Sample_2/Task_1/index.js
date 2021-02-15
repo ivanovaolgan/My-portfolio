e564c1505eddd77dcabf38b738f4231c7824a7d1
ivanovaolgan <45678236+ivanovaolgan@users.noreply.github.com>
2021-02-15 15:28:15 -0500

    Delete Sample_2 directory

--- Sample_2/Task_1/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {String} tweet
- * @returns {String[]}
- */
-module.exports = function (tweet) {
-	return tweet.split(' ').filter(hashtag => hashtag.startsWith('#')==true).map(element => element.slice(1));
-};
